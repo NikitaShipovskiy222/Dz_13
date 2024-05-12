5f2fa89ae68ed1f2d32e4f3a32fede9f6bf7fb33
NikitaShipovskiy222 <nikita04.shipovskiy@gmail.com>
2024-05-12 12:15:59 +0300

    Initial Commit

---
+++ Dz_13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dz_13
+//
+//  Created by Nikita Shipovskiy on 12/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
